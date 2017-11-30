d65a3e72bdb3085c927c0f634f86f72857efb9f0
Bartiff <bartiff@gmail.com>
2017-11-30 15:02:14 +0100

    Mise à jour de speedtest-cli vers v1.0.7

--- bin/speedtest.sh
+++ bin/speedtest.sh
@@ -7,14 +7,14 @@ echo $path
 if [ -f $path/ressources/graphdebits-$year.json ]
 then
     #echo "Le fichier existe"
-   speed=`python $path/bower_components/speedtest/index.py --server 5559 --json`
+   speed=`python $path/bower_components/speedtest-cli/speedtest.py --server 5559 --json`
    sed -i '$d' $path/ressources/graphdebits-$year.json
    echo ','$speed >> $path/ressources/graphdebits-$year.json
    echo ']' >> $path/ressources/graphdebits-$year.json
 else
     #echo "Le fichier n'existe pas"
    echo "[\n{}" > $path/ressources/graphdebits-$year.json
-   speed=`python $path/bower_components/speedtest/index.py --server 5559 --json`
+   speed=`python $path/bower_components/speedtest-cli/speedtest.py --server 5559 --json`
    echo ','$speed >> $path/ressources/graphdebits-$year.json
    echo ']' >> $path/ressources/graphdebits-$year.json
 fi
